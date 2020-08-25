e2648f6e268d557a8bac991f08d0ff22507c1ddf
Andreas Nes Kile <andreasnk95@gmail.com>
2020-08-25 17:29:52 +0200

    Styling to introduction section and skeleton for experience section

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { fas, faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
-import "./App.css";
+import "./App.scss";
 import Introduction from "./components/introduction";
+import Projects from "./components/projects";
 
 library.add(fab, fas, faGithub, faLinkedin, faChevronDown);
 
@@ -29,66 +30,99 @@ function App() {
 
 	return (
 		<div className="App">
-			<div className="content-wrapper">
-				<div className="uk-position-center-left uk-light dotnav">
-					<ul className="uk-dotnav uk-dotnav-vertical" id="sections">
-						{sections.map((section, i) => (
-							<li
-								className={
-									activeSection.sectionName ===
-									section.sectionName
-										? "uk-active"
-										: null
-								}
-								key={i}
-							>
-								<a
-									href={"#" + section.sectionName}
-									aria-label={section.sectionName}
-									onClick={() => setActiveSection(section)}
-									uk-tooltip={
-										"title: " +
-										section.sectionName.toUpperCase() +
-										"; pos: right"
+			<div className="main-content uk-section uk-section-small">
+				<div className="content-wrapper">
+					<div className="uk-position-center-left uk-light dotnav">
+						<ul
+							className="uk-dotnav uk-dotnav-vertical"
+							id="sections"
+						>
+							{sections.map((section, i) => (
+								<li
+									className={
+										activeSection.sectionName ===
+										section.sectionName
+											? "uk-active"
+											: null
 									}
-								></a>
-							</li>
-						))}
-					</ul>
-				</div>
-				<div className="uk-section">
-					<div
-						className="uk-container uk-container-large introduction-container uk-animation-slide-top-medium"
-						id="introduksjon"
-					>
-						<Introduction />
+									key={i}
+								>
+									<a
+										href={"#" + section.sectionName}
+										aria-label={section.sectionName}
+										onClick={() =>
+											setActiveSection(section)
+										}
+										uk-tooltip={
+											"title: " +
+											section.sectionName.replace(
+												"-",
+												" "
+											) +
+											"; pos: right; cls: uk-active " +
+											section.sectionName +
+											" uk-dotnav-categories"
+										}
+									></a>
+								</li>
+							))}
+						</ul>
+					</div>
+					<div className="uk-section">
+						<div
+							className="uk-container introduction-container uk-animation-slide-top-medium"
+							id="introduksjon"
+						>
+							<Introduction />
+						</div>
+					</div>
+					<div className="uk-section project-section">
+						<div className="uk-container projects-container">
+							<Projects />
+						</div>
 					</div>
 				</div>
-			</div>
-			<div className="footer" id="sosiale-medier">
-				<div className="socials-wrapper">
-					<li className="socials-list">
-						<a
-							href="https://www.linkedin.com/in/andreasneskile"
-							target="_"
-						>
-							<span
-								className="uk-icon-link socials-linkedin"
-								uk-icon="icon: linkedin; ratio: 1.5"
-							></span>
-						</a>
-					</li>
-					<li>
-						<a href="https://github.com/AndreasNesKile" target="_">
-							<span
-								className="uk-icon-link socials-github"
-								uk-icon="icon: github; ratio: 1.5"
-							></span>
-						</a>
-					</li>
+				<div className="uk-container to-the-top-container">
+					<a
+						href="/"
+						uk-totop="true"
+						uk-scroll="true"
+						uk-tooltip={
+							"title: Til toppen; pos: top; cls: uk-active to-the-top-button"
+						}
+						className="to-top-button uk-position-center"
+					></a>
 				</div>
-				<div className="copyright">
-					Håndlaget av meg | {new Date().getFullYear()}
+			</div>
+			<div className="footer-wrapper">
+				<div className="footer" id="sosiale-medier">
+					<div className="socials-wrapper">
+						<li className="socials-list">
+							<a
+								href="https://www.linkedin.com/in/andreasneskile"
+								target="_"
+							>
+								<span
+									className="uk-icon-link socials-linkedin"
+									uk-icon="icon: linkedin; ratio: 1.5"
+								></span>
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://github.com/AndreasNesKile"
+								target="_"
+							>
+								<span
+									className="uk-icon-link socials-github"
+									uk-icon="icon: github; ratio: 1.5"
+								></span>
+							</a>
+						</li>
+					</div>
+					<div className="copyright">
+						Håndlaget av meg | {new Date().getFullYear()}
+					</div>
 				</div>
 			</div>
 		</div>
--- src/components/introduction.tsx
+++ src/components/introduction.tsx
@@ -6,9 +6,20 @@ const Introduction = () => (
 	<div className="introduction-section">
 		<div className="text">
 			Hei, jeg heter
-			<span className="highlight"> Andreas Nes Kile.</span>
+			<span className="highlight">Andreas Nes Kile.</span>
 			<br />
-			Jeg er en full-stack utvikler.
+			<div className="break"></div>
+			<div className="i-am-container">
+				Jeg er en
+				<div className="fullstack-wrapper ">
+					<div className="junior-text uk-badge">junior</div>
+					<div className="full-p uk-animation-slide-top">full</div>
+					<div className="stack-p uk-animation-slide-bottom">
+						stack
+					</div>
+				</div>
+				utvikler.
+			</div>
 		</div>
 	</div>
 );
